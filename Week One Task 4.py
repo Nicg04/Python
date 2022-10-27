dff201c5dab13f550dd7a9da78844ac8b11c7325
nic <mrnicholasgraham@hotmail.com>
2022-10-27 20:19:21 +0100

    Work

---
+++ Week One Task 4.py
@@ -0,0 +1,8 @@
+print("In a long career for Yorkshire, "
+      "Geoffrey Boycott played 609 matches, "
+      "batted 1014times, was not out 162 times, and scored 48426 runs. "
+      "Write a program to calculateand display Boycott's batting average.")
+x = 48426 / (1014 - 162)
+
+
+print("His batting average is :" + str(x))
